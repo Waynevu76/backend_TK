f8a5569821dfcce4d2e0f93667246e31c06efd94
Vũ Quang <quangvu7690@gmail.com>
2019-02-18 17:28:24 +0700

    fix

--- modules/api/auth/router.js
+++ modules/api/auth/router.js
@@ -3,6 +3,15 @@ const router = express.Router();
 
 const authController = require("./controller");
 
+router.get("/", (req, res) => {
+  const { userInfo } = req.session;
+  if (userInfo &&  userInfo.username) {
+    res.json(userInfo);
+  } else {
+    res.status(401).json("Not logging in.");
+  }
+});
+
 router.post("/", (req, res) => {
   authController
     .login(req.body)
